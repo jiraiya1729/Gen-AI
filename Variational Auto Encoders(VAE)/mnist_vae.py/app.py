160460cf9ff26688c50944e857dad7cec07cec62
jiraiya1729 <komminenivarun1729@gmail.com>
2024-04-27 23:42:29 +0530

    vae

---
+++ Variational Auto Encoders(VAE)/mnist_vae.py/app.py
@@ -0,0 +1,17 @@
+import torch 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+class VAEmodel(nn.Module):
+    super(VAEmodel, self).__init__()
+
+    
+    
+def train_vae():
+    pass
+
+
+if __name__='__main__':
+    train_vae()
